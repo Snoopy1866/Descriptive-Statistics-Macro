369456f6991811429db163b77b3d334eef627d04
Firefly <45487685+Snoopy1866@users.noreply.github.com>
2025-11-14 17:15:09 +0800

    perf(ci): auto remove sas file which is not found in gbk (#108)

--- multi_encoding_converter.py
+++ multi_encoding_converter.py
@@ -10,12 +10,19 @@ if __name__ == "__main__":
 
     # 开始转换
     base_path = Path("src") / base_encode
+    base_files = {file.name for file in base_path.glob("*.sas")}
+    
     for convert_encode in convert_encode_list:
         convert_path = Path("src") / convert_encode
         if not convert_path.exists():
             convert_path.mkdir(parents=True)
+        
+        # 删除 base_path 中已不存在的文件
+        for file in convert_path.glob("*.sas"):
+            if file.name not in base_files:
+                file.unlink()
+        
+        # 复制或更新 base_path 中的文件
         for file in base_path.glob("*.sas"):
             content = file.read_text(encoding=base_encode)
-            (Path(convert_path) / file.name).write_text(
-                content, encoding=convert_encode
-            )
+            (Path(convert_path) / file.name).write_text(content, encoding=convert_encode)
